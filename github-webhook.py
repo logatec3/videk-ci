b018b1f6b66cb82c70911355c11e5c98056bf679
Matevz Vucnik <vucnikm@gmail.com>
2017-09-11 11:10:12 +0200

    add ping support

--- github-webhook.py
+++ github-webhook.py
@@ -10,11 +10,12 @@ app = Flask(__name__)
 @app.route('/pyload', methods=['POST'])
 def webhook():
 	data = json.loads(request.data)
-	tag_name = data['release']['tag_name']
-	upload_url = data['release']['upload_url'].replace("{?name,label}", "")
-	repository_name = data['repository']['name']
-	repository_url = data['repository']['clone_url']
-	subprocess.Popen(["./releasebuilder", tag_name, upload_url, repository_name, repository_url, github_token])
+	if 'hook_id' not in data:
+		tag_name = data['release']['tag_name']
+		upload_url = data['release']['upload_url'].replace("{?name,label}", "")
+		repository_name = data['repository']['name']
+		repository_url = data['repository']['clone_url']
+		subprocess.Popen(["./releasebuilder", tag_name, upload_url, repository_name, repository_url, github_token])
 	return "OK"
 
 if __name__ == '__main__':
--- test/github-mockup.py
+++ test/github-mockup.py
@@ -1,9 +1,11 @@
 import json
 import requests
 
-with open('release-sample.json') as data_file:    
+with open('hook-sample.json') as data_file:
     data = json.load(data_file)
     
 headers = {'Content-type': 'application/json'}
 
 r = requests.post("http://localhost:8000/pyload", data=json.dumps(data), headers=headers)
+
+print r.text
